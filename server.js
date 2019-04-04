2d037cc712d1547c55806ce7f5b90136d74da9ec
simarpreet-singh <simsi@ucdavis.edu>
2019-04-04 16:52:47 -0700

    add portfolio

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
   res.render("index", {
         title: "data science & economics | simarpreet singh ",
+        active_home:"active",
         active_about:"",
         active_projects:"",
         active_contact:""
@@ -19,26 +20,64 @@ app.get('/', (req, res) => {
 
 // -- PORTFOLIO --
 app.get('/portfolio', (req, res) => {
-  res.render("portfolio", {
-        title: "data science portfolio | simarpreet singh",
+  res.redirect('/#portfolio')
+});
+// Redirect /projects to /portfolio
+app.get('/projects', (req, res) => {
+  res.redirect('/#portfolio')
+});
+
+// portfolio projects
+
+//Digital Marketing
+app.get('/portfolio/digital-marketing-analytics', (req, res) => {
+  res.render("projects/digital-marketing-analytics", {
+        title: "digital marketing analytics | simarpreet singh",
+        active_home:"",
         active_about:"",
         active_projects:"active",
         active_contact:""
     })
 });
-// Redirect /projects to /portfolio
-app.get('/projects', (req, res) => {
-  res.redirect('/portfolio')
+
+//KNN
+app.get('/portfolio/knn', (req, res) => {
+  res.render("projects/knn", {
+        title: "nearest neighbor algorithm | simarpreet singh",
+        active_home:"",
+        active_about:"",
+        active_projects:"active",
+        active_contact:""
+    })
 });
+
+//CNN
+app.get('/portfolio/ccn', (req, res) => {
+  res.render("projects/ccn", {
+        title: "community cell networks | simarpreet singh",
+        active_home:"",
+        active_about:"",
+        active_projects:"active",
+        active_contact:""
+    })
+});
+
+//SAHF Tour
+app.get('/portfolio/music', (req, res) => {
+  res.render("projects/music", {
+        title: "sun and her flowers tour - rupi kaur | simarpreet singh",
+        active_home:"",
+        active_about:"",
+        active_projects:"active",
+        active_contact:""
+    })
+});
+
+
 
 // -- ABOUT --
 app.get('/about', (req, res) => {
-  res.render("about", {
-        title: "about & resume | simarpreet singh",
-        active_about:"active",
-        active_projects:"",
-        active_contact:""
-    })
+  res.redirect('/#about')
 });
 // Redirect /resume to /about
 app.get('/resume', (req, res) => {
@@ -49,6 +88,7 @@ app.get('/resume', (req, res) => {
 app.get('/contact', (req, res) => {
   res.render("contact", {
         title: "contact | simarpreet singh",
+        active_home:"",
         active_about:"",
         active_projects:"",
         active_contact:"active"
@@ -59,6 +99,7 @@ app.get('/contact', (req, res) => {
 app.get('/blog', (req, res) => {
   res.render("blog", {
         title: "blog | simarpreet singh",
+        active_home:"",
         active_about:"",
         active_projects:"",
         active_contact:"",
